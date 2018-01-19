c148e8f43052e4ee567420c72a70cac9043bae8b
Ellis Mwekunda <Scareb15@gmail.com>
2018-01-19 14:59:43 -0700

    bfs and dfs algorithim

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,46 +1,58 @@
-const Node = require('./Node')
-const Queue = require('./Queue')
+const Node = require("./Node");
+const Queue = require("./Queue");
 
 class BinarySearchTree {
-	constructor() {
-		this.root = null
-	}
+  constructor() {
+    this.root = null;
+  }
 
-	insert(data) {
-		if(!this.root) {
-			this.root = new Node(data)
-		} else {
-			let currentNode = this.root
+  insert(data) {
+    if (!this.root) {
+      this.root = new Node(data);
+    } else {
+      let currentNode = this.root;
 
-			while(true) {
-				if(data <= currentNode.data) {
-					if(currentNode.left) {
-						currentNode = currentNode.left
-					} else {
-						currentNode.left = new Node(data)
-						break
-					}
-				} else {
-					if(currentNode.right) {
-						currentNode = currentNode.right
-					} else {
-						currentNode.right = new Node(data)
-						break
-					}
-				}
-			}
-		}
-	}
+      while (true) {
+        if (data <= currentNode.data) {
+          if (currentNode.left) {
+            currentNode = currentNode.left;
+          } else {
+            currentNode.left = new Node(data);
+            break;
+          }
+        } else {
+          if (currentNode.right) {
+            currentNode = currentNode.right;
+          } else {
+            currentNode.right = new Node(data);
+            break;
+          }
+        }
+      }
+    }
+  }
 
-	dfs(cb, node=this.root) {
-    // Write an algorithm using depth first search that will take in a callback function and call it while passing each nodes data starting down the left path
-		// Expected order: '25, 15, 17, 197, 97, 9997'
-	}
+  dfs(cb, node = this.root) {
+	if(!node)return;
+	cb(node.data)
+	this.dfs(cb, node.left)
+	this.dfs(cb, node.right)
+  }
 
-	bfs(cb, node=this.root) {
-    // Write an algorithm using breadth first search that takes in a callback function and calls it for every node
-    // Expected order: 25, 15, 197, 17, 97, 9997
-	}
+
+  bfs(cb, node = this.root) {
+    let arr = [node];
+    while (arr.length > 0) {
+      let curr = arr.shift();
+      cb(curr.data);
+      if (curr.left) {
+        arr.push(curr.left);
+      }
+      if (curr.right) {
+        arr.push(curr.right);
+      }
+    }
+  }
 }
 
-module.exports = BinarySearchTree
+module.exports = BinarySearchTree;
